62ce6ba2121fb112c731a8d18f3ee36ef0e8f2a8
Srijeet Chatterjee <champ.srijeet@gmail.com>
2020-07-31 18:02:20 +0530

    Added path for pip in jenkins file

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def hello_world():
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
-    return 'Srijeet welcomes you my friend %s!\n' % username
+    return 'Gargi & srijeet welcomes you my friend %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
