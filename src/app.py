48c452ba5f24b42cac16a059b3dc39cf7541df3e
Srijeet Chatterjee <champ.srijeet@gmail.com>
2020-08-01 23:59:53 +0530

    update for medium article

--- src/app.py
+++ src/app.py
@@ -6,11 +6,11 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!\n'
+    return 'Hello into the world of DevOps!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
-    return 'Gargi & srijeet welcomes you my friend %s!\n' % username
+    return 'I have been Updated my friend %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
